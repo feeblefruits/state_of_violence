bf47da2137ac11f8bb76a4c08e60fe5989b3bfb2
feeblefruits <j.coetzee0@gmail.com>
2019-09-10 17:24:32 +0200

    Added murder matrix chart
    Need to add proper container, responsive, interactive

---
+++ assets/js/charts/murder_matrix.js
@@ -0,0 +1,62 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 40, bottom: 30, left: 140},
+  width = 800 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// Labels of row and columns
+var myYears = ['1995','1996','1997','1998','1999','2000','2001','2002',
+               '2003','2004','2005','2006','2007','2008','2009','2010','2011',
+               '2012','2013','2014','2015','2016']
+
+var myCountries = ['Virgin Islands (U.S.)','Dominican Republic','Greenland',"Cote d'Ivoire",
+                'Guatemala','Puerto Rico','Panama','Colombia','Lesotho','El Salvador','Russian Federation','Paraguay',
+                'Kazakhstan','Jamaica','Venezuela, RB','Guyana','Ecuador','Trinidad and Tobago','Honduras',
+                'Central African Republic','St. Kitts and Nevis','Seychelles','Curacao','St. Vincent and the Grenadines',
+                'Botswana','Small states','Congo, Dem. Rep.','Suriname','Nicaragua','Mali','St. Lucia','Mauritania','South Sudan',
+                'Mexico','Eswatini','Brazil','Bahamas, The','Belize','South Africa','Namibia']
+
+// Build X scales and axis:
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(myYears)
+  .padding(0.01);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+
+// Build X scales and axis:
+var y = d3.scaleBand()
+  .range([ height, 0 ])
+  .domain(myCountries)
+  .padding(0.01);
+svg.append("g")
+  .call(d3.axisLeft(y));
+
+// Build color scale
+var myColor = d3.scaleLinear()
+  .range(["white", "#8b0000"])
+  .domain([1,100])
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/feeblefruits/state_of_violence/body-development/assets/data/country_murder_rates_matrix.csv", function(data) {
+
+  svg.selectAll()
+      .data(data, function(d) {return d.year+':'+d.country_name;})
+      .enter()
+      .append("rect")
+      .attr("x", function(d) { return x(d.year) })
+      .attr("y", function(d) { return y(d.country_name) })
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.murder_rate)} )
+
+})
